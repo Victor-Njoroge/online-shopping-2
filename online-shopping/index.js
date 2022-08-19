5fb8827021cf0edbbc536675d2ea8d1ce2bf5681
Victor Njoroge <86822599+Victor-Njoroge@users.noreply.github.com>
2022-08-19 11:35:42 +0300

    Add files via upload
    This is a small project which involves an online shopping platform where one can purchase goods

---
+++ online-shopping/index.js
@@ -0,0 +1,64 @@
+function add(){
+    var num1=parseInt(document.getElementById("num7").value);
+
+    ++num1;
+    document.getElementById("num7").value=num1;
+    console.log(num1);
+    var num3=document.getElementById("num8").value=899*num1;
+}
+add();
+function subtract(){
+    var num2=parseInt(document.getElementById("num7").value);
+    --num2;
+    document.getElementById("num7").value=num2;
+    console.log(num2);
+    var num3=document.getElementById("num8").value=899*num2;
+}
+function add1(){
+    var num1=parseInt(document.getElementById("num7").value);
+
+    ++num1;
+    document.getElementById("num7").value=num1;
+    console.log(num1);
+    var num3=document.getElementById("num8").value=2599*num1;
+}
+add1();
+function subtract1(){
+    var num2=parseInt(document.getElementById("num7").value);
+    --num2;
+    document.getElementById("num7").value=num2;
+    console.log(num2);
+    var num3=document.getElementById("num8").value=2599*num2;
+}
+function add2(){
+    var num1=parseInt(document.getElementById("num7").value);
+
+    ++num1;
+    document.getElementById("num7").value=num1;
+    console.log(num1);
+    var num3=document.getElementById("num8").value=1599*num1;
+}
+add2();
+function subtract2(){
+    var num2=parseInt(document.getElementById("num7").value);
+    --num2;
+    document.getElementById("num7").value=num2;
+    console.log(num2);
+    var num3=document.getElementById("num8").value=1599*num2;
+}
+function add3(){
+    var num1=parseInt(document.getElementById("num7").value);
+
+    ++num1;
+    document.getElementById("num7").value=num1;
+    console.log(num1);
+    var num3=document.getElementById("num8").value=2599*num1;
+}
+add3();
+function subtract3(){
+    var num2=parseInt(document.getElementById("num7").value);
+    --num2;
+    document.getElementById("num7").value=num2;
+    console.log(num2);
+    var num3=document.getElementById("num8").value=2599*num2;
+}
